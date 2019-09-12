25d897dc4c07a007d99a59067c787ea835d9c2b7
Jason Ruan <jasonruan@pursuit.org>
2019-09-12 10:18:48 -0400

    Copies over code from SeriesFetchingClient, adjusts to EpisodesFetchingClient

--- Unit 3 Project 2/Models/EpisodesFetchingClient.swift
+++ Unit 3 Project 2/Models/EpisodesFetchingClient.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+class EpisodesFetchingClient {
+    private init() {}
+    static let manager = EpisodesFetchingClient()
+    
+    func getShowEpisodes(showID: Int, completionHandler: @escaping (Result<[ShowEpisodes], AppError>) -> () ) {
+        let showEpisodesURLString = "http://api.tvmaze.com/shows/\(showID)/episodes"
+        NetworkHelper.shared.fetchData(urlString: showEpisodesURLString) { (result) in
+            switch result {
+            case .failure:
+                completionHandler(.failure(.noDataError))
+            case .success(let data):
+                do {
+                    let allEpisodes = try JSONDecoder().decode([ShowEpisodes].self, from: data)
+                    completionHandler(.success(allEpisodes))
+                } catch {
+                    completionHandler(.failure(.badJSONError))
+                }
+            }
+        }
+    }
+    
+}
