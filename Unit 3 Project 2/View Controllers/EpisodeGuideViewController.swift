24f1b7d1345971c993416ff1d2f4b33600e91197
Jason Ruan <jasonruan@pursuit.org>
2019-09-12 17:18:24 -0400

    Defines cellForRowAt function in the EpisodeGuideVC

--- Unit 3 Project 2/View Controllers/EpisodeGuideViewController.swift
+++ Unit 3 Project 2/View Controllers/EpisodeGuideViewController.swift
@@ -22,22 +22,53 @@ class EpisodeGuideViewController: UIViewController, UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        showTableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        if chosenSeriesEpisodes != nil {
+            return chosenSeriesEpisodes.max { a, b in a.season < b.season }!.season
+        } else {
+            return 1
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //TODO
-        return 1
+        if chosenSeriesEpisodes != nil {
+            return chosenSeriesEpisodes.filter { $0.season == (section + 1) }.count
+        } else {
+            return 1
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //TODO
-        return UITableViewCell()
+        guard chosenSeriesEpisodes != nil else { return UITableViewCell() }
+        guard let cell = showTableView.dequeueReusableCell(withIdentifier: "episodeCell", for: indexPath) as? EpisodeTableViewCell else { return UITableViewCell() }
+        var episode: ShowEpisodes?
+        for ep in chosenSeriesEpisodes where ep.season == (indexPath.section + 1) && ep.episode == (indexPath.row + 1) {
+            episode = ep
+        }
+        guard episode != nil else { return UITableViewCell() }
+        cell.episodeTitleLabel.text = episode!.name
+        cell.episodeSeasonAndEpisodeLabel.text = "S:\(episode!.season) E:\(episode!.episode)"
+        if let episodeImageURL = episode!.image?.originalImage {
+            ImageHelper.manager.getImage(imageURL: episodeImageURL) { (result) in
+                DispatchQueue.main.async {
+                    switch result {
+                    case .failure(let error):
+                        print(error)
+                    case .success(let episodeImage):
+                        cell.episodeImage.image = episodeImage
+                    }
+                }
+            }
+        }
+        return cell
     }
     
     // MARK: - Navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-      
-    }
-
+    //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    //
+    //    }
+    
 }
