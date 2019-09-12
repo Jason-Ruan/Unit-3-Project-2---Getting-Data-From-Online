49dcc7605fc193a4cbfbef67ae8eab6d0c9844c5
Jason Ruan <jasonruan@pursuit.org>
2019-09-12 17:14:37 -0400

    Adds searchByQuery function in SeriesFetchingClient

--- Unit 3 Project 2/Models/SeriesFetchingClient.swift
+++ Unit 3 Project 2/Models/SeriesFetchingClient.swift
@@ -29,4 +29,22 @@ class SeriesFetchingClient {
         }
     }
     
+    func getQuerySearch(searchEntry: String, completionHandler: @escaping (Result<[Series], AppError>) -> () ) {
+        let queryURLString = "http://api.tvmaze.com/search/shows?q=\(searchEntry)"
+        NetworkHelper.shared.fetchData(urlString: queryURLString) { (result) in
+            switch result {
+            case .failure:
+                completionHandler(.failure(.noDataError))
+            case .success(let data):
+                do {
+                    let arrSeriesWrappers = try JSONDecoder().decode([SeriesWrapper].self, from: data)
+                    let searchedSeries = arrSeriesWrappers.map { $0.show }
+                    completionHandler(.success(searchedSeries))
+                } catch {
+                    completionHandler(.failure(.badJSONError))
+                }
+            }
+        }
+    }
+    
 }
