a7616ec08ade9175dc5d6e16029b509456199a6c
Jason Ruan <jasonruan@pursuit.org>
2019-09-12 20:31:18 -0400

    Adjusts cellForRowAt in EpisodeGuideVC to not make sections from season value

--- Unit 3 Project 2/View Controllers/EpisodeGuideViewController.swift
+++ Unit 3 Project 2/View Controllers/EpisodeGuideViewController.swift
@@ -27,32 +27,26 @@ class EpisodeGuideViewController: UIViewController, UITableViewDataSource {
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        if chosenSeriesEpisodes != nil {
-            return chosenSeriesEpisodes.max { a, b in a.season < b.season }!.season
-        } else {
-            return 1
-        }
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let chosenSeriesName = chosenSeriesName else { return "N/A" }
+        return chosenSeriesName
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if chosenSeriesEpisodes != nil {
-            return chosenSeriesEpisodes.filter { $0.season == (section + 1) }.count
-        } else {
-            return 1
-        }
+        guard chosenSeriesEpisodes != nil else { return 1 }
+        return chosenSeriesEpisodes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard chosenSeriesEpisodes != nil else { return UITableViewCell() }
         guard let cell = showTableView.dequeueReusableCell(withIdentifier: "episodeCell", for: indexPath) as? EpisodeTableViewCell else { return UITableViewCell() }
-        var episode: ShowEpisodes?
-        for ep in chosenSeriesEpisodes where ep.season == (indexPath.section + 1) && ep.episode == (indexPath.row + 1) {
-            episode = ep
-        }
-        guard episode != nil else { return UITableViewCell() }
-        cell.episodeTitleLabel.text = episode!.name
-        cell.episodeSeasonAndEpisodeLabel.text = "S:\(episode!.season) E:\(episode!.episode)"
-        if let episodeImageURL = episode!.image?.originalImage {
+        let episode = chosenSeriesEpisodes[indexPath.row]
+        cell.episodeTitleLabel.text = episode.name
+        cell.episodeSeasonAndEpisodeLabel.text = "S:\(episode.season) E:\(episode.episode)"
+        if let episodeImageURL = episode.image?.originalImage {
             ImageHelper.manager.getImage(imageURL: episodeImageURL) { (result) in
                 DispatchQueue.main.async {
                     switch result {
